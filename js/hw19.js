4433709e8bfc45cb4a518e094465bde526e7a8e3
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2022-07-19 15:49:44 +0300

    first commit

---
+++ js/hw19.js
@@ -0,0 +1,17 @@
+'use strict'
+const arr = [1, 2, 3, -1, -2, -3];
+
+const toPos = (arr) => {
+    const exampleArr = []
+    for (let i = 0; i < arr.length; i++) {
+        const number = arr[i]
+        if (number > 0) {
+            exampleArr.push(number)
+        }
+     }
+    if(exampleArr.length === 0) {
+        return null
+    }
+    return exampleArr
+}
+console.log(toPos(arr));
