b56161024b79576a0fa268917082bab87a8c4f32
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2022-07-21 23:18:01 +0300

    remake

--- js/hw18.js
+++ js/hw18.js
@@ -1,6 +1,16 @@
 'use strict'
 const padStr = (str, length, symbol, boolean = true) => {
     let result = ""
+    if (typeof str !== 'string') throw new Error('This isn\'t a string');
+    if (typeof length !== 'number') throw new Error('This isn\'t a number');
+
+    if (symbol.length > 1) {
+        return false
+    } else if (length < str.length) {
+        return str.substring(0, length)
+    } else if (!str || !length || !symbol) {
+        return false
+    }
 
     for (let i = 0; i < length; i++) {
         result += symbol
@@ -10,13 +20,9 @@ const padStr = (str, length, symbol, boolean = true) => {
         retResult = (str + result).slice(0, length)
     }
 
-    if (symbol.length > 1) {
-        return false
-    } else if (length < str.length) {
-        return str.substr(0, length)
-    } else if(!str || !length || !symbol) {
-        return false
-    }
+    if (typeof symbol !== 'string') throw new Error('This isn\'t a string');
+    if (typeof boolean !== 'boolean') throw new Error('This isn\'t a boolean');
+
     return retResult
 }
-console.log(padStr('JS', 10 , '*', false))
+console.log(padStr('JS', 10,'*'))
